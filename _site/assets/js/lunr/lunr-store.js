fcf37ebe780da8c9b74996418902de96b7cfc1b2
boworkgo <bowork189@gmail.com>
2019-04-27 13:12:20 -0400

    Op wk not secret no more

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,11 +1,16 @@
 var store = [{
-        "title": "Hello World",
-        "excerpt":"Created this blog  ","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/2019/04/06/Hello-World",
-        "teaser":null},{
         "title": "A Great Day",
         "excerpt":"4-10-2019 12:44 AM I learned so much stuff on my own today. Unfortunately, I know this means I’ll be disappointed tomorrow when I can’t reach what I did today. Nevertheless, I had the most intense hour learning OS concepts and enjoyed some great moments. Let me recap. Let’s begin with...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/2019/04/09/A-Great-Day",
+        "teaser":null},{
+        "title": "PlaidCTF 2019 Writeup",
+        "excerpt":"4-15-2019 12:10 AM Docker solution.sh sudo docker pull whowouldeverguessthis/public sudo docker images sudo docker image inspect whowouldeverguessthis/public sudo docker ps -a sudo docker run -it --name testPCTF whowouldeverguessthis/public bash grep -rI --exclude-dir=proc --exclude-dir=sys 'PCTF{' . cd /var/lib/docker/ sudo grep -rI 'PCTF{' . I pulled the docker image (immutable object). Then,...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2019/04/15/PlaidCTF-2019-Writeup",
+        "teaser":null},{
+        "title": "Mark_time",
+        "excerpt":"1:10 PM 4-27-2019   On Peru, Everson’s rocket atttracted tumbling ions. On Nigeria, who knows?  ","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2019/04/27/Final",
         "teaser":null}]
