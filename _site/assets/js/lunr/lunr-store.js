24e0c824212e83b26527e3b642d357f32752502d
boworkgo <bowork189@gmail.com>
2019-07-29 22:48:15 -0400

    OSCP post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,11 +1,11 @@
 var store = [{
-        "title": "A Great Day",
-        "excerpt":"4-10-2019 12:44 AM I learned so much stuff on my own today. Unfortunately, I know this means I’ll be disappointed tomorrow when I can’t reach what I did today. Nevertheless, I had the most intense hour learning OS concepts and enjoyed some great moments. Let me recap. Let’s begin with...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/2019/04/09/A-Great-Day",
-        "teaser":null},{
         "title": "PlaidCTF 2019 Writeup",
-        "excerpt":"4-15-2019 12:10 AM Docker solution.sh sudo docker pull whowouldeverguessthis/public sudo docker images sudo docker image inspect whowouldeverguessthis/public sudo docker ps -a sudo docker run -it --name testPCTF whowouldeverguessthis/public bash grep -rI --exclude-dir=proc --exclude-dir=sys 'PCTF{' . cd /var/lib/docker/ sudo grep -rI 'PCTF{' . I pulled the docker image (immutable object). Then,...","categories": [],
+        "excerpt":"4-15-2019 12:10 AM Docker Just so you know, or I might be wrong: a docker image is the immutable source code, and docker container is an image’s running instance. I pulled the challenge image and tried to grep for the flag in the container. When that didn’t work, I searched...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/2019/04/15/PlaidCTF-2019-Writeup",
+        "teaser":null},{
+        "title": "OSCP Advice",
+        "excerpt":"7-29-2019 9:48 PM My exam is fewer than three weeks from today. It is so scary because I don’t feel prepared in the slightest. Hopefully that will change soon. To give me some hope of my success I’m writing about I’ve learned so far from the labs: OneNote is great...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/2019/07/29/OSCP-Update",
         "teaser":null}]
