1c5a446abf0ec8625345cbabad4d22c758c09a2b
boworkgo <bowork189@gmail.com>
2019-11-04 20:07:12 -0500

    Remove old post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,9 +3,4 @@ var store = [{
         "excerpt":"4-15-2019 12:10 AM Docker Just so you know, or I might be wrong: a docker image is the immutable source code, and docker container is an image’s running instance. I pulled the challenge image and tried to grep for the flag in the container. When that didn’t work, I searched...","categories": [],
         "tags": [],
         "url": "http://localhost:4000/2019/04/15/PlaidCTF-2019-Writeup",
-        "teaser":null},{
-        "title": "OSCP Advice",
-        "excerpt":"8-29-2019 8:48 PM One month update: I didn’t pass the OSCP on August 17. I’ll be trying again December during winter break. This semester will be the final one in which I will broaden my view of computer science and security through random projects; then I’m going to specialize in...","categories": [],
-        "tags": [],
-        "url": "http://localhost:4000/2019/07/29/OSCP-Update",
         "teaser":null}]
